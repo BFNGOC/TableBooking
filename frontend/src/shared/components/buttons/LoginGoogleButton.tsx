8de5aa510f66a3fda53c8d2c7e580469734ddd5e
2280603415-HuynhKimTrong <tronghuynh1609@gmail.com>
2026-07-12 13:02:31 +0700

    fix: implement Google login

--- frontend/src/shared/components/buttons/LoginGoogleButton.tsx
+++ frontend/src/shared/components/buttons/LoginGoogleButton.tsx
@@ -2,10 +2,18 @@
 
 import { Button } from "@heroui/react";
 import { Icon } from "@iconify/react";
+import { signIn } from "next-auth/react";
 
-function LoginGoogleButton({ onClick }: { onClick: () => void }) {
+function LoginGoogleButton() {
+	const handleGoogleLogin = async () => {
+		await signIn("google", {
+			callbackUrl: "/",
+			redirect: true,
+		});
+	};
+
 	return (
-		<Button className="w-full" variant="tertiary" onPress={onClick}>
+		<Button className="w-full" variant="tertiary" onPress={handleGoogleLogin}>
 			<Icon icon="devicon:google" />
 			Sign in with Google
 		</Button>
