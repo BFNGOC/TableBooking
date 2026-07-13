fdd0f7c3a7a53ac578cd9beae05adc7ad0805056
BFNGOC <takanashirikka041012@gmail.com>
2026-07-13 13:17:00 +0700

    fix: redirect login gogle

--- frontend/src/shared/components/buttons/LoginGoogleButton.tsx
+++ frontend/src/shared/components/buttons/LoginGoogleButton.tsx
@@ -1,23 +1,23 @@
-"use client";
+'use client';
 
-import { Button } from "@heroui/react";
-import { Icon } from "@iconify/react";
-import { signIn } from "next-auth/react";
+import { Button } from '@heroui/react';
+import { Icon } from '@iconify/react';
+import { signIn } from 'next-auth/react';
 
 function LoginGoogleButton() {
-	const handleGoogleLogin = async () => {
-		await signIn("google", {
-			callbackUrl: "/",
-			redirect: true,
-		});
-	};
+    const handleGoogleLogin = async () => {
+        await signIn('google', {
+            callbackUrl: '/google-callback',
+            redirect: true,
+        });
+    };
 
-	return (
-		<Button className="w-full" variant="tertiary" onPress={handleGoogleLogin}>
-			<Icon icon="devicon:google" />
-			Sign in with Google
-		</Button>
-	);
+    return (
+        <Button className="w-full" variant="tertiary" onPress={handleGoogleLogin}>
+            <Icon icon="devicon:google" />
+            Sign in with Google
+        </Button>
+    );
 }
 
 export default LoginGoogleButton;
