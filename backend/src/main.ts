43f4813e44e992f3beb5510601acb0930b2d0b63
BFNGOC <takanashirikka041012@gmail.com>
2026-09-08 13:42:08 +0700

    feat: add security headers

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? 8080;
