2446312d0ae561307451d15ca991d663f69842fd
BFNGOC <takanashirikka041012@gmail.com>
2026-09-13 15:35:51 +0700

    feat: k6

---
+++ k6/restaurant-list.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 25,
+    duration: '30s',
+
+    thresholds: {
+        http_req_failed: ['rate<0.01'],
+        http_req_duration: ['p(95)<1000'],
+    },
+};
+
+export default function () {
+    const res = http.get('http://localhost:8080/api/v1/restaurants?currentPage=1&pageSize=10');
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+}
